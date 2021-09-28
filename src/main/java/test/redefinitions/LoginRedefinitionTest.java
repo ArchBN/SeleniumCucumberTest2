b06af8aad94943bdbcd90cf5fbfa4399f89eb402
ArchBN <archana.narayana@gmail.com>
2021-09-28 19:30:13 +0530

    Added Logon logo verification

---
+++ src/main/java/test/redefinitions/LoginRedefinitionTest.java
@@ -0,0 +1,55 @@
+package test.redefinitions; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* LoginRedefinition Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sep 28, 2021</pre> 
+* @version 1.0 
+*/ 
+public class LoginRedefinitionTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: I_launch_Chrome_browser() 
+* 
+*/ 
+@Test
+public void testI_launch_Chrome_browser() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: I_open_Orange_HRM_page() 
+* 
+*/ 
+@Test
+public void testI_open_Orange_HRM_page() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: Close_the_browser() 
+* 
+*/ 
+@Test
+public void testClose_the_browser() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
